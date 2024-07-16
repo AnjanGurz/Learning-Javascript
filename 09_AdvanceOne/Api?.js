85e295601d501122828275428ca468534b9feac6
Anjan Gurung <anjangurz@gmail.com>
2024-07-16 05:03:20 -0400

    updated with xmlHttpReq

---
+++ 09_AdvanceOne/Api?.js
@@ -0,0 +1,16 @@
+/*
+    How to Talk:
+        Protocol: APIs establish a set of rules or protocols that determine how different software (built on diff technologies like(java, c# etc)) components should communicate with each other. This includes specifying the format of requests and responses, the methods to be used (GET, POST, PUT, DELETE, etc.), and how errors should be handled.
+        Data Exchange: APIs facilitate the sending and receiving of data between systems. This data can be in various formats, such as JSON, XML, or plain text.
+
+    How to Be Lazy and Delegate Work:
+        Reuse of Functionality: Instead of building everything from scratch, APIs allow developers to use existing functionalities provided by other services. For example, you can use an API to integrate weather data, payment processing, or social media sharing into your application.
+        Interoperability: APIs enable different software systems, possibly written in different programming languages, to work together. This interoperability is crucial for building complex systems that leverage the strengths of various technologies and services.
+
+Example:
+
+Imagine you are building a website that needs to display weather information. Instead of collecting and processing weather data yourself, you can use an API provided by a weather service. You send a request to the weather API, and it returns the data you need, which you then display on your website. This way, you save time and resources by utilizing the work done by the weather service.
+
+This approach allows developers to focus on the unique aspects of their projects while leveraging the capabilities provided by other services through APIs.
+
+*/
