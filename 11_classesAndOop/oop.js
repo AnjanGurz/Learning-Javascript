87c10c1fecc0278088a35028e2dbc13c14f59c1f
Anjan Gurung <138888260+AnjanGurz@users.noreply.github.com>
2024-07-22 19:54:46 -0400

    Update oop.js

--- 11_classesAndOop/oop.js
+++ 11_classesAndOop/oop.js
@@ -38,8 +38,14 @@ console.log(userTwo);
 // about new key word concept
 // when we use new keyword it creates the empty object  and it helps to a prevents fields from overriding data
 
-console.log(userOne.constructor);
+console.log(userOne.constructor); // output: User
 /*
+Think of .constructor like a "birth certificate" for an object.
+It tells you:
+
+    Who created me? (the constructor function or class)
+    Where did I come from? (the original blueprint or template)
+    
 In JavaScript, (.constructor) is a property of an object that refers to the function that created the instance of that object.
 It's a way to identify the constructor function or class that was used to create the object.
 */
@@ -49,12 +55,12 @@ It's a way to identify the constructor function or class that was used to create
 
 **************instanceof***************
 
-The instanceof operator in JavaScript is used to check whether an object is an instance of a specific class or constructor function.
-It allows you to determine the type of an object and ensure it conforms to a particular structure or behavior.
+Instance of
+In JavaScript, instanceof is an operator that checks if an object is an instance of a particular constructor function or class.
 
 Syntax:
 
-object instanceof Constructor
+object instanceof Constructor // ouput : true/false 
 
     object: The object you want to test.
     Constructor: The function or class you want to test against.
@@ -64,6 +70,27 @@ How It Works
 The instanceof operator checks the prototype chain of the object to see if it contains the Constructor's prototype.
 If it does, the result is true; otherwise, it's false.
 
+Example using Function:
+
+        function Car() {}
+        
+        And you create an object myCar using that constructor:
+        JavaScript
+        
+        let myCar = new Car();
+        
+        To check if myCar is an instance of Car, you can use instanceof
+        
+        console.log(myCar instanceof Car); // Output: true
+
+Example using Class: 
+    class Vehicle {}
+    class Car extends Vehicle {}
+    
+    let myCar = new Car();
+    
+    console.log(myCar instanceof Car); // Output: true
+    console.log(myCar instanceof Vehicle); // Output: true
 
 
 */
